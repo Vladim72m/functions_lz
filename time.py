ab7eea868482ee015f37758d929feacffb740de9
Vladim72m <vladim72dx@tutamail.com>
2025-11-27 15:47:05 +0300

    Add files via upload

---
+++ time.py
@@ -0,0 +1,8 @@
+a = int(input('часы:'))
+b = int(input('минуты:'))
+c = int(input('секунды:'))
+def sum(a, b, c):
+    d = a * 3600
+    i  b * 60=
+    return (d + i + c)
+print(sum(a, b, c), 'секунд')
