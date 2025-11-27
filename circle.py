15bacf6581efe6cda6efc456e1fd96d2c07f07cd
Vladim72m <vladim72dx@tutamail.com>
2025-11-27 20:07:50 +0300

    circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,10 @@
-a = int(input('Радиус круга:'))
-def operation(a):
-    b = a * 2 * 3.14
-    c = 3.14 * a * a
-    return ('площадь круга:', c, 'длина окружности', b)
-print (operation(a))
+def operation(a):   # функция вывода
+    b = a * 2 * 3.14 # длина окружности
+    c = 3.14 * a * a # площадь круга
+    print('площадь круга:', c, 'длина окружности', b)
+
+def main():
+    a = int(input('Радиус круга:')) # ввод радиуса
+    operation(a)
+if __name__ == "__main__":
+    main()
