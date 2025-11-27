fc6c07a08f615caa8c007b217ec48925d701b1e5
Vladim72m <vladim72dx@tutamail.com>
2025-11-27 20:18:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import circle
+import temperature
+import is_even
+import timee
+def main():
+    circle.main()
+    temperature.main()
+    is_even.main()
+    timee.main()
+
+if __name__ == "__main__":
+    main()
