4c987eaf5aa1807b71716b028ef3b22b3d3c6c7a
Vladim72m <vladim72dx@tutamail.com>
2025-11-27 20:10:36 +0300

    timee.py

--- time.py
+++ time.py
@@ -1,8 +1,13 @@
-a = int(input('часы:'))
-b = int(input('минуты:'))
-c = int(input('секунды:'))
-def sum(a, b, c):
-    d = a * 3600
-    i  b * 60=
-    return (d + i + c)
-print(sum(a, b, c), 'секунд')
+def operation(a, b, c):
+    return ((a * 3600) + (b * 60) + c) # количество секунд
+
+def main():
+    a = int(input('часы:'))   # ввод часов
+    b = int(input('минуты:'))  # ввод минут
+    c = int(input('секунды:'))  # ввод секунд
+    abc = operation(a, b, c)
+    print("В секундах:", abc)
+    
+if __name__ == "__main__":
+    main()
+    
